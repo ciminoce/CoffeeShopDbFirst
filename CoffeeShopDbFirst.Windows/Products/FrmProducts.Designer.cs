63704f24e9d438cdd493a00a62f83431ce362f81
ciminoce <ciminoce@yahoo.com.ar>
2021-06-04 15:41:56 -0300

    Formularios de Productos

---
+++ CoffeeShopDbFirst.Windows/Products/FrmProducts.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace CoffeeShopDbFirst.Windows.Products
+{
+    partial class FrmProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.NewToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.DeleteToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.UpdateToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ProductsDataGridView = new System.Windows.Forms.DataGridView();
+            this.colProduct = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewToolStripButton,
+            this.DeleteToolStripButton,
+            this.UpdateToolStripButton,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(924, 62);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // NewToolStripButton
+            // 
+            this.NewToolStripButton.Image = global::CoffeeShopDbFirst.Windows.Properties.Resources.new_copy_40px;
+            this.NewToolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.NewToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.NewToolStripButton.Name = "NewToolStripButton";
+            this.NewToolStripButton.Size = new System.Drawing.Size(44, 59);
+            this.NewToolStripButton.Text = "New";
+            this.NewToolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // DeleteToolStripButton
+            // 
+            this.DeleteToolStripButton.Image = global::CoffeeShopDbFirst.Windows.Properties.Resources.delete_document_40px;
+            this.DeleteToolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.DeleteToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DeleteToolStripButton.Name = "DeleteToolStripButton";
+            this.DeleteToolStripButton.Size = new System.Drawing.Size(44, 59);
+            this.DeleteToolStripButton.Text = "Delete";
+            this.DeleteToolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // UpdateToolStripButton
+            // 
+            this.UpdateToolStripButton.Image = global::CoffeeShopDbFirst.Windows.Properties.Resources.edit_property_40px;
+            this.UpdateToolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.UpdateToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.UpdateToolStripButton.Name = "UpdateToolStripButton";
+            this.UpdateToolStripButton.Size = new System.Drawing.Size(49, 59);
+            this.UpdateToolStripButton.Text = "Update";
+            this.UpdateToolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 62);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ProductsDataGridView);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(924, 451);
+            this.panel1.TabIndex = 1;
+            // 
+            // ProductsDataGridView
+            // 
+            this.ProductsDataGridView.AllowUserToAddRows = false;
+            this.ProductsDataGridView.AllowUserToDeleteRows = false;
+            this.ProductsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colProduct,
+            this.colType,
+            this.colPrice});
+            this.ProductsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProductsDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.ProductsDataGridView.MultiSelect = false;
+            this.ProductsDataGridView.Name = "ProductsDataGridView";
+            this.ProductsDataGridView.ReadOnly = true;
+            this.ProductsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ProductsDataGridView.Size = new System.Drawing.Size(924, 451);
+            this.ProductsDataGridView.TabIndex = 0;
+            // 
+            // colProduct
+            // 
+            this.colProduct.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colProduct.HeaderText = "Product";
+            this.colProduct.Name = "colProduct";
+            this.colProduct.ReadOnly = true;
+            // 
+            // colType
+            // 
+            this.colType.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colType.HeaderText = "Product Type";
+            this.colType.Name = "colType";
+            this.colType.ReadOnly = true;
+            // 
+            // colPrice
+            // 
+            this.colPrice.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colPrice.HeaderText = "Price";
+            this.colPrice.Name = "colPrice";
+            this.colPrice.ReadOnly = true;
+            this.colPrice.Width = 56;
+            // 
+            // FrmProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 513);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FrmProducts";
+            this.Text = "FrmProducts";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton NewToolStripButton;
+        private System.Windows.Forms.ToolStripButton DeleteToolStripButton;
+        private System.Windows.Forms.ToolStripButton UpdateToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView ProductsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProduct;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+    }
+}
---
+++ CoffeeShopDbFirst.Windows/Products/FrmProductsEdit.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace CoffeeShopDbFirst.Windows.Products
+{
+    partial class FrmProductsEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProductTypesComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FullPriceTextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Description:";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(133, 48);
+            this.DescriptionTextBox.MaxLength = 50;
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(415, 20);
+            this.DescriptionTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Product Type:";
+            // 
+            // ProductTypesComboBox
+            // 
+            this.ProductTypesComboBox.FormattingEnabled = true;
+            this.ProductTypesComboBox.Location = new System.Drawing.Point(133, 92);
+            this.ProductTypesComboBox.Name = "ProductTypesComboBox";
+            this.ProductTypesComboBox.Size = new System.Drawing.Size(415, 21);
+            this.ProductTypesComboBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Full Price:";
+            // 
+            // FullPriceTextBox
+            // 
+            this.FullPriceTextBox.Location = new System.Drawing.Point(133, 138);
+            this.FullPriceTextBox.MaxLength = 50;
+            this.FullPriceTextBox.Name = "FullPriceTextBox";
+            this.FullPriceTextBox.Size = new System.Drawing.Size(415, 20);
+            this.FullPriceTextBox.TabIndex = 1;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Image = global::CoffeeShopDbFirst.Windows.Properties.Resources.ok_28px;
+            this.OkButton.Location = new System.Drawing.Point(49, 220);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(121, 58);
+            this.OkButton.TabIndex = 3;
+            this.OkButton.Text = "OK";
+            this.OkButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.OkButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.Image = global::CoffeeShopDbFirst.Windows.Properties.Resources.cancel_28px;
+            this.CancelarButton.Location = new System.Drawing.Point(427, 220);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(121, 58);
+            this.CancelarButton.TabIndex = 3;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            // 
+            // FrmProductsEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 313);
+            this.ControlBox = false;
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.ProductTypesComboBox);
+            this.Controls.Add(this.FullPriceTextBox);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(613, 352);
+            this.MinimumSize = new System.Drawing.Size(613, 352);
+            this.Name = "FrmProductsEdit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmProductsEdit";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ProductTypesComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FullPriceTextBox;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button CancelarButton;
+        private System.Windows.Forms.Button OkButton;
+    }
+}
